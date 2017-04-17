b27939636f662d7273ad010fcbc762359850e44e
griffin96 <griffin96@naver.com>
2017-04-17 18:01:32 +0900

    5일차 작업

---
+++ src/javatutorials/DefineDemo.java
@@ -0,0 +1,15 @@
+package javatutorials;
+
+public class DefineDemo {
+
+	public static void main(String[] args)  {
+		
+		String[] classGroup = {"최강혁","김성기","조윤호","이준석"};  // 값들은 원소
+		
+		System.out.println(classGroup[0]);
+		System.out.println(classGroup[1]);
+		System.out.println(classGroup[2]);
+		System.out.println(classGroup[3]);  // 숫자는 index, 색인이라고 함
+	}
+
+}
---
+++ src/javatutorials/LengthDemo.java
@@ -0,0 +1,22 @@
+package javatutorials;
+
+public class LengthDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       String[] classGroup = new String[4];
+       
+       classGroup[0] ="최진혁";
+       System.out.println(classGroup.length);  // length는 원소의 갯수, 4라는 것은 변수에 담겨있는 몇개의 값을 수용하는지 알려줌
+       
+       classGroup[0] ="최유빈";
+       System.out.println(classGroup.length);
+       
+       classGroup[0] ="한이람";
+       System.out.println(classGroup.length);
+       
+       classGroup[0] ="이고잉";
+       System.out.println(classGroup.length);
+	}
+
+}
