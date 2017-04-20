eabf4ef925b16509f79fabac1bb01c5d5108cbbf
griffin96 <griffin96@naver.com>
2017-04-20 17:49:24 +0900

    목요일 스터디

---
+++ src/javatutorials/AvgDemo.java
@@ -0,0 +1,14 @@
+package javatutorials;
+
+public class AvgDemo {
+
+	  public static void avg(int left , int right ){		
+			System.out.println((left+right)/2);
+		}
+	      public static void main(String[] args) {
+	  		// TODO Auto-generated method stub
+	       avg(10,20);
+	       avg(20,40);
+}
+		
+		}
