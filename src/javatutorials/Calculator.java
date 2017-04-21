57c122de1092d54684318d17052253fbc144316e
griffin96 <griffin96@naver.com>
2017-04-21 17:53:36 +0900

    금요일 스터디

---
+++ src/javatutorials/Calculator.java
@@ -0,0 +1,18 @@
+package javatutorials;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Calculator {
+
+	@Test
+	public void testSum() {
+		// fail("Not yet implemented");
+		
+		
+		    }
+		}
+	}
+
+}
---
+++ src/javatutorials/CalculatorDemo4Test.java
@@ -0,0 +1,14 @@
+package javatutorials;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CalculatorDemo4Test {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
