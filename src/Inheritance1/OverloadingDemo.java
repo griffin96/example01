fa18fab930565917535cdf43185b3f4263ec8dbf
griffin96 <griffin96@naver.com>
2017-04-26 17:43:36 +0900

    수요일

---
+++ src/Inheritance1/OverloadingDemo.java
@@ -0,0 +1,18 @@
+package Inheritance1;
+
+public class OverloadingDemo {
+    void A (){System.out.println("void A()");}
+    void A (int arg1){System.out.println("void A (int arg1)");}
+    void A (String arg1){System.out.println("void A (String arg1)");}
+    
+    // int A (){System.out.println("void A()");}  오버라이딩은 상속관계
+    
+    public static void main(String[] args) {
+        
+    	OverloadingDemo od = new OverloadingDemo();
+        
+    	od.A();
+        od.A(1);
+        od.A("coding everybody");
+    }
+}
